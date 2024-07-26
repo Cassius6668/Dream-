ea73bf57ae2fbaa3c60391e4ac19cb02c15eec94
cassius <1278935776@qq.com>
2024-07-26 23:00:53 +0800

    Initial Commit

---
+++ Dream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dream
+//
+//  Created by 程俊哲 on 2024/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dream/DreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamApp.swift
+//  Dream
+//
+//  Created by 程俊哲 on 2024/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
